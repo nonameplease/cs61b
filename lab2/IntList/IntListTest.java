80ac6755ce98e29e6d0e5fddb0ac4c749cc156c4
Scott Shao <shaos@berkeley.edu>
2017-08-30 12:49:36 -0700

    Finished lab2

--- lab2/IntList/IntListTest.java
+++ lab2/IntList/IntListTest.java
@@ -35,6 +35,13 @@ public class IntListTest {
      */
 
     //TODO:  Create testSquareListRecursive()
+    @Test
+    public void testSquareListRecursive(){
+        IntList L = IntList.list(1,2,3);
+        IntList LSquare = IntList.squareListRecursive(L);
+        assertEquals(IntList.list(1,4,9), LSquare);
+        assertNotEquals(LSquare, L);
+    }
 
     /* Run the unit tests in this file. */
     public static void main(String... args) {
