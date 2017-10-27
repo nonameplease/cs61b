67c18b415b2cf42a2405495e98a10a54f28c72f2
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2017-10-27 13:51:51 -0700

    Update hw6

---
+++ hw6/ECHashStringSet.java
@@ -0,0 +1,11 @@
+// REPLACE THIS STUB WITH THE CORRECT SOLUTION.
+// The current contents of this file are merely to allow things to compile 
+// out of the box. It bears scant relation to a proper solution (for one thing,
+// a hash table should not be a SortedStringSet.)
+/** A set of String values.
+ *  @author
+ */
+class ECHashStringSet extends BSTStringSet {
+
+
+}
