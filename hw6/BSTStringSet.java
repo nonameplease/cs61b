c7419eb14c33d3bb6c9453b9f88bc6660993cde4
Scott Shao <shaos@berkeley.edu>
2017-11-03 00:21:15 -0700

    hw6-2

--- hw6/BSTStringSet.java
+++ hw6/BSTStringSet.java
@@ -1,8 +1,4 @@
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Implementation of a BST based String Set.
@@ -104,7 +100,7 @@ public class BSTStringSet implements SortedStringSet, Iterable<String> {
          *  then (c) the nodes in the rest of the stack (i.e., the result
          *  of recursively applying this rule to the result of popping
          *  the stack. */
-        private Stack<Node> _toDo = new Stack<>();
+        private ArrayDeque<Node> _toDo = new ArrayDeque<>();
 
         private String _low, _high;
 
@@ -125,7 +121,7 @@ public class BSTStringSet implements SortedStringSet, Iterable<String> {
 
         @Override
         public boolean hasNext() {
-            return !_toDo.empty();
+            return !_toDo.isEmpty();
         }
 
         @Override
--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,41 +1,71 @@
-// REPLACE THIS STUB WITH THE CORRECT SOLUTION.
-// The current contents of this file are merely to allow things to compile 
-// out of the box. It bears scant relation to a proper solution (for one thing,
-// a hash table should not be a SortedStringSet.)
 
 import java.util.LinkedList;
 import java.util.List;
 
 /** A set of String values.
- *  @author
+ *  @author Scott Shao
  */
 class ECHashStringSet implements StringSet {
+
+    /**
+     * Maximum load factor. This could also used to
+     * calculate minimum load factor.
+     */
     private static final int MAX_LOAD_FACTOR = 5;
-    private static final double MIN_LOAD_FACTOR = 0.2;
+
+    /**
+     * The bit operation to get rid of sign bit.
+     */
     private static final int REMOVE_TOP_BIT = 0x7fffffff;
+
+    /**
+     * The default starting bucket size.
+     */
     private static final int STARTING_SIZE = 5;
 
-    private LinkedList<String>[] buckets;
+    /**
+     * A LinkedList of LinkedList of String to store buckets.
+     */
+    private LinkedList<LinkedList<String>> buckets;
+
+    /**
+     * The total number of arguments in the ECH.
+     */
     private int size;
+
+    /**
+     * A boolean to keep track if resizing is in progress.
+     */
     private boolean resizing = false;
 
+    /**
+     * Constructor with bucket size.
+     * @param bucketsSize The size of the bucket.
+     */
     public ECHashStringSet(int bucketsSize) {
-        buckets = new LinkedList[bucketsSize];
+        buckets = new LinkedList<LinkedList<String>>();
         size = 0;
-        for (int i = 0; i < buckets.length; i += 1) {
-            buckets[i] = new LinkedList<String>();
+        for (int i = 0; i < bucketsSize; i += 1) {
+            buckets.add(new LinkedList<String>());
         }
     }
 
+    /**
+     * Default constructor.
+     */
     public ECHashStringSet() {
         this(STARTING_SIZE);
     }
 
+    /**
+     * Resize the bucket.
+     * @param newCount New bucket number.
+     */
     public void resize(int newCount) {
         ECHashStringSet newechss = new ECHashStringSet(newCount);
         newechss.resizing = true;
-        for (int i = 0; i < buckets.length; i += 1) {
-            for (String s : buckets[i]) {
+        for (int i = 0; i < buckets.size(); i += 1) {
+            for (String s : buckets.get(i)) {
                 newechss.put(s);
             }
         }
@@ -43,33 +73,51 @@ class ECHashStringSet implements StringSet {
         newechss.resizing = false;
     }
 
+    /**
+     * Adds the string S to the string set. If it is already present in the
+     * set, do nothing.
+     * @param s A String.
+     */
     public void put(String s) {
         size += 1;
-        int bucketNum = buckets.length;
-        if (size == 1) {
-
-        } else if (size > bucketNum * MAX_LOAD_FACTOR && !resizing) {
+        int bucketNum = buckets.size();
+        if (size != 1 && size > bucketNum * MAX_LOAD_FACTOR && !resizing) {
             resize(bucketNum * 5);
-        } else if (size < bucketNum / MAX_LOAD_FACTOR && !resizing) {
+        } else if (size != 1
+                && size < bucketNum / MAX_LOAD_FACTOR && !resizing) {
             resize(bucketNum / 5);
         }
 
         int hashCode = s.hashCode() & REMOVE_TOP_BIT;
         int whichBucket = hashCode % bucketNum;
-        LinkedList<String> temp = buckets[whichBucket];
+        LinkedList<String> temp = buckets.get(whichBucket);
         if (!temp.contains(s)) {
             temp.add(s);
         }
     }
 
+    /**
+     * Returns true iff S is in the string set.
+     * @param s A string argument.
+     * @return true iff S is in the string set.
+     */
     public boolean contains(String s) {
+        int bucketNum = buckets.size();
+        int hashCode = s.hashCode() & REMOVE_TOP_BIT;
+        int whichBucket = hashCode % bucketNum;
+        LinkedList<String> temp = buckets.get(whichBucket);
+        //return temp.contains(s);
         return this.contains(s);
     }
 
+    /**
+     * Return a list of all members of this set in ascending order.
+     * @return a list of all members of this set in ascending order.
+     */
     public List<String> asList() {
         List<String> returnValue = new LinkedList<String>();
-        for (int i = 0; i < buckets.length; i += 1) {
-            for (String s : buckets[i]) {
+        for (int i = 0; i < buckets.size(); i += 1) {
+            for (String s : buckets.get(i)) {
                 returnValue.add(s);
             }
         }
