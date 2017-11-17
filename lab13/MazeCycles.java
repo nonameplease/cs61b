62a6a829137bbbe734c151b3deeb4744bb5f2e57
Scott Shao <shaos@berkeley.edu>
2017-11-16 20:55:52 -0800

    lab13

--- lab13/MazeCycles.java
+++ lab13/MazeCycles.java
@@ -9,16 +9,42 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private Maze maze;
+    private boolean cycle;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = 0;
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        findCycle(maze, s, s);
     }
 
     // Helper methods go here
+    private void findCycle(Maze maze, int v, int p) {
+        marked[v] = true;
+        announce();
+        if (cycle) {
+            return;
+        }
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                marked[w] = true;
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                findCycle(maze, w, v);
+            } else if (w != p) {
+                cycle = true;
+            }
+        }
+    }
 }
 
